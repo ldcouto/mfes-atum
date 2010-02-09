4af265b39266cf76a361ad874aabbb2470ca8e54
ti.veloso <ti.veloso@5327595c-bfbc-11de-ad03-79fce359684b>
2010-02-09 15:09:18 +0000

    Alguns toques nos contracts

--- fase2/deliver/ATUM/ATUM/sistema/Aluno.cs
+++ fase2/deliver/ATUM/ATUM/sistema/Aluno.cs
@@ -19,7 +19,7 @@ namespace ATUM.sistema
         public String Identifier { get; set; }
 
         /// <summary>
-        /// Lista de Disciplinas a que o Aluno está inscrito.
+        /// Lista de Disciplinas a que o Aluno está inscricoes.
         /// </summary>
         public IList<Disciplina> Inscrito { get; private set; }
 
@@ -48,16 +48,19 @@ namespace ATUM.sistema
         /// Indica a posição do Aluno na ordem do sistema.
         /// </summary>
         public uint NumOrdem { get; set; }
-        
+
         #endregion
 
         #region Construtores
         /// <summary>
         /// Construtor de aluno mais simples. Apenas gera um novo aluno sem relações a nada.
         /// </summary>
-        /// <param name="id">Identificação do Aluno.</param>
-        public Aluno(String id) {
-            Identifier = id;
+        /// <param name="identifier">Identificação do Aluno.</param>
+        public Aluno(String identifier)
+        {
+            Contract.Requires<ArgumentNullException>(!String.IsNullOrEmpty(identifier), "O nome do Aluno não pode ser nulo nem vazio.");
+
+            Identifier = identifier;
 
             Inscrito = new List<Disciplina>();
             AlocadoTurno = new List<Turno>();
@@ -72,12 +75,16 @@ namespace ATUM.sistema
         /// <summary>
         /// Construtor de aluno standard.
         /// </summary>
-        /// <param name="id">Identificação do Aluno.</param>
-        /// <param name="insc">Lista de Disciplinas a que o aluno está inscrito.</param>
-        public Aluno(String id, IList<Disciplina> insc) {
-            Identifier = id;
+        /// <param name="identifier">Identificação do Aluno.</param>
+        /// <param name="inscricoes">Lista de Disciplinas a que o aluno está inscricoes.</param>
+        public Aluno(String identifier, IList<Disciplina> inscricoes)
+        {
+            Contract.Requires<ArgumentNullException>(!String.IsNullOrEmpty(identifier), "O nome do Aluno não pode ser nulo nem vazio.");
+            Contract.Requires<ArgumentNullException>(inscricoes != null, "As inscrições do aluno devem ser uma lista válida.");
 
-            Inscrito = insc;
+            Identifier = identifier;
+
+            Inscrito = inscricoes;
             AlocadoTurno = new List<Turno>();
 
             //PreferenciasBlocos = new List<Bloco>();
@@ -91,12 +98,17 @@ namespace ATUM.sistema
         /// Construtor completo de aluno
         /// </summary>
         /// <param name="identifier">Identificação do Aluno.</param>
-        /// <param name="inscrito">Lista de Disciplinas a que o aluno está inscrito.</param>
+        /// <param name="inscricoes">Lista de Disciplinas a que o aluno está inscricoes.</param>
         /// <param name="preferenciasBlocos">Preferências de Blocos do Aluno.</param>
-        public Aluno(string identifier, IList<Disciplina> inscrito, IList<Preferencia> preferenciasBlocos) {
+        public Aluno(String identifier, IList<Disciplina> inscricoes, IList<Preferencia> preferenciasBlocos)
+        {
+            Contract.Requires<ArgumentNullException>(!String.IsNullOrEmpty(identifier), "O nome do Aluno não pode ser nulo nem vazio.");
+            Contract.Requires<ArgumentNullException>(inscricoes != null, "As inscrições do aluno devem ser uma lista existente.");
+            Contract.Requires<ArgumentNullException>(preferenciasBlocos != null, "As preferências do Aluno devem ser uma lista existente.");
+
             Identifier = identifier;
 
-            Inscrito = inscrito;
+            Inscrito = inscricoes;
             AlocadoTurno = new List<Turno>();
 
             PreferenciasBlocos = preferenciasBlocos;
@@ -106,24 +118,13 @@ namespace ATUM.sistema
         }
         #endregion
 
-        #region Métodos da Classe
-        /// <summary>
-        /// Adiciona uma Disciplina às inscrições do Aluno.
-        /// </summary>
-        /// <param name="d">Disciplina a adicionar.</param>
-        public void AddInscricao(Disciplina d) {
-            Contract.Requires(!Inscrito.Contains(d));
-            Contract.Ensures(Inscrito.Contains(d));
-
-            //if (!Inscrito.Contains(d))
-            Inscrito.Add(d);
-        }
+        #region Métodos Estáticos
         /// <summary>
         /// Comparador entre Alunos pelo seu número de ordem.
         /// </summary>
         /// <param name="x">Aluno x a comparar.</param>
         /// <param name="y">Aluno y a comparar.</param>
-        /// <returns>0 se iguais; -1 se x maior, 1 se y maior.</returns>
+        /// <returns>0 se iguais; 1 se x maior, -1 se y maior.</returns>
         [Pure]
         public static int CompareAlunosByOrd(Aluno x, Aluno y)
         {
@@ -135,19 +136,33 @@ namespace ATUM.sistema
                 return 1;
             return x.NumOrdem.CompareTo(y.NumOrdem);
         }
+        #endregion
+
+        #region Métodos da Classe
+        /// <summary>
+        /// Adiciona uma Disciplina às inscrições do Aluno.
+        /// </summary>
+        /// <param name="d">Disciplina a adicionar.</param>
+        public void AddInscricao(Disciplina d)
+        {
+            Contract.Requires(!Inscrito.Contains(d));
+            Contract.Ensures(Inscrito.Contains(d));
+
+            Inscrito.Add(d);
+        }
 
         /// <summary>
         /// Remove uma Disciplina das inscrições do Aluno.
         /// </summary>
         /// <param name="d">Disciplina a remover.</param>
         /// <returns></returns>
-        public bool RemoveInscricao(Disciplina d) {
+        public bool RemoveInscricao(Disciplina d)
+        {
             Contract.Requires(Inscrito.Contains(d));
             Contract.Ensures(!Inscrito.Contains(d));
 
             return Inscrito.Remove(d);
         }
-
 
         //TODO refazer este método!
         ///// <summary>
@@ -161,13 +176,15 @@ namespace ATUM.sistema
         //    //if (!PreferenciasBlocos.Contains(b))
         //    PreferenciasBlocos.Add(b);
         //}
+
         /// <summary>
         /// Adiciona um Turno aos alocados do Aluno
         /// </summary>
         /// <param name="t">Turno a adicionar</param>
-        public void AddAlocacaoTurno(Turno t) {
-            Contract.Requires(!AlocadoTurno.Contains(t));
-            Contract.Requires(AlocadoTurno != null);
+        public void AddAlocacaoTurno(Turno t)
+        {
+            Contract.Requires(t != null, "O turno a inserir tem de existir.");
+            Contract.Requires(!AlocadoTurno.Contains(t), "O aluno não pode já estar alocado ao turno a inserir. ");
             Contract.Ensures(AlocadoTurno.Contains(t));
 
             AlocadoTurno.Add(t);
@@ -177,36 +194,37 @@ namespace ATUM.sistema
 
         #region Invariantes
         [ContractInvariantMethod]
-        private void ObjectInvariant() {
+        private void ObjectInvariant()
+        {
             // Garantir que se um aluno está alocado num bloco, então está alocado a todos os turnos dele
             Contract.Invariant(!(AlocadoBloco != null) ||
                                Enumerable.Intersect(AlocadoBloco.TurnosBloco, AlocadoTurno) == AlocadoBloco.TurnosBloco);
 
-            // Garantir que um aluno só tem preferências por blocos para os quais está inscrito a todas as disciplinas
-//            Contract.Invariant(Contract.ForAll(PreferenciasBlocos, (Bloco b) 
-//                => Contract.ForAll(b.TurnosBloco, (Turno t) => Inscrito.Contains(t.Disciplina))) );
-            Contract.Invariant( Contract.ForAll(PreferenciasBlocos, (Preferencia p) => 
-                Contract.ForAll(p.Bloco.TurnosBloco, (Turno t) => Inscrito.Contains(t.Disciplina))) );
+            // Garantir que um aluno só tem preferências por blocos para os quais está inscricoes a todas as disciplinas
+            //Contract.Invariant(Contract.ForAll(PreferenciasBlocos, (Bloco b) 
+            //    => Contract.ForAll(b.TurnosBloco, (Turno t) => Inscrito.Contains(t.Disciplina))) );
+            Contract.Invariant(Contract.ForAll(PreferenciasBlocos, (Preferencia p) =>
+                Contract.ForAll(p.Bloco.TurnosBloco, (Turno t) => Inscrito.Contains(t.Disciplina))));
 
 
             // Garantir que un aluno não processado não é alocado
-            Contract.Invariant(this.Processado || this.AlocadoTurno.Count==0 && this.AlocadoBloco == null);
-            
-            // Garantir que um aluno só é alocado se estiver inscrito 
+            Contract.Invariant(this.Processado || this.AlocadoTurno.Count == 0 && this.AlocadoBloco == null);
+
+            // Garantir que um aluno só é alocado se estiver inscricoes 
             // Garantir que um aluno não é alocado a Turnos "inúteis"
             // (Estes dois eram mais um workaround ao Alloy)
-            
+
             // Garantir que um Aluno apenas é alocado em Turnos de Disciplinas em que está matriculado
             Contract.Invariant(Contract.ForAll(AlocadoTurno, (Turno t) => Inscrito.Contains(t.Disciplina) && t.Disciplina != null));
-            
+
             // Um aluno não pode estar alocado em turnos sobre opostos
-//           Contract.Invariant(Contract.ForAll(AlocadoTurno, (Turno t1)
-//               => Contract.ForAll(AlocadoTurno, (Turno t2) => t1 == t2 || !t1.Sobreposto(t2))));
+            //Contract.Invariant(Contract.ForAll(AlocadoTurno, (Turno t1)
+            //    => Contract.ForAll(AlocadoTurno, (Turno t2) => t1 == t2 || !t1.Sobreposto(t2))));
             Contract.Invariant(StructOps.NaoTemDups(AlocadoTurno.Select(x => x.Spot).ToList()));
 
             // Um aluno não tem o mesmo grau de preferencia por dois blocos diferentes
-            Contract.ForAll(PreferenciasBlocos, p1 
-                => Contract.ForAll(PreferenciasBlocos, p2 
+            Contract.ForAll(PreferenciasBlocos, p1
+                => Contract.ForAll(PreferenciasBlocos, p2
                     => (p1 == p2 || (p1.Grau != p2.Grau && p1.Bloco != p2.Bloco))));
         }
         #endregion
