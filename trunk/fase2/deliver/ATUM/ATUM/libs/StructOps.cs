519a93809831bbb8d640e2a4c5dc34844a57acc6
ldcouto <ldcouto@5327595c-bfbc-11de-ad03-79fce359684b>
2010-02-09 15:03:33 +0000

    Movi as funcs de manipulação de listas para uma classe própria

---
+++ trunk/fase2/deliver/ATUM/ATUM/libs/StructOps.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using ATUM;
+using ATUM.sistema;
+
+namespace ATUM.libs {
+    public static class StructOps {
+
+        /// <summary>
+        /// Método auxiliar para verificar a existência de duplicados numa lista.
+        /// </summary>
+        /// <param name="l">Lista a testar.</param>
+        /// <returns>True caso não haja duplicados False caso contrário.</returns>
+        [Pure]
+        public static bool NaoTemDups(IList l) {
+            for (int i = 0; i < l.Count; i++)
+                for (int j = 0; j < l.Count; j++)
+                    if (i != j && l[i] == l[j])
+                        return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Método auxiliar para verificar que uma lista está ordenada.
+        /// </summary>
+        /// <param name="l">A lista a verificar.</param>
+        /// <returns>True caso a lista esteja ordenada. Falso caso contrário.</returns>
+        [Pure]
+        public static bool IsSorted(IList<int> l) {
+            for (int i = 0; i < l.Count; i++)
+                for (int j = i; j < l.Count; j++)
+                    if (i != j && l[i] >= l[j])
+                        return false;
+            return true;
+        }
+        [Pure]
+        public static bool IsSorted(IList<uint> l) {
+            for (int i = 0; i < l.Count; i++)
+                for (int j = i; j < l.Count; j++)
+                    if (i != j && l[i] >= l[j])
+                        return false;
+            return true;
+        }
+
+
+
+        /// <summary>
+        /// Método auxiliar para construir um mapa a partir da lista de Alunos incritos no sistema.
+        /// </summary>
+        /// <returns>Um mapa de pares (Posição na Lista; Número de Ordem).</returns>
+        [Pure]
+        public static Dictionary<int, uint> GenMap(IList<Aluno> l) {
+            var r = new Dictionary<int, uint>();
+            int i = 1;
+            foreach (var aluno in l) {
+                r.Add(i, aluno.NumOrdem);
+                i++;
+            }
+            return r;
+        }
+
+    }
+}
