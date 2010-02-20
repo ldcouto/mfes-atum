becd6586abe265b3c18f6e7f4063994a5aa0efe9
ti.veloso <ti.veloso@5327595c-bfbc-11de-ad03-79fce359684b>
2010-02-20 17:08:40 +0000

    Mais alguns retoques nos contractos.

--- fase2/deliver/ATUM/ATUM/sistema/Turno.cs
+++ fase2/deliver/ATUM/ATUM/sistema/Turno.cs
@@ -90,6 +90,12 @@ namespace ATUM.sistema
         {
             Contract.Requires<ArgumentNullException>(outro != null, "O turno contra o qual se quer testar não pode ser nulo.");
             //Contract.Requires<ArgumentException>(outro != this, "O turno contra o qual se quer testar não pode ser o mesmo turno.");
+            
+            Contract.Ensures(Contract.Result<bool>() == (Spot == outro.Spot), "Garante que o turno ou está sobreposto ou não.");
+            //Será Preciso?
+            //Contract.Ensures(Contract.OldValue(this) == this && Contract.OldValue(outro) == outro, "Nada é alterado no decorrer deste método.");
+
+            Contract.EnsuresOnThrow<ArgumentNullException>(Contract.OldValue(this) == this && Contract.OldValue(outro) == outro);
 
             return Spot == outro.Spot;
         }
